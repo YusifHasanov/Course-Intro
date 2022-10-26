dc8b47ac0f78964b15a50a8d306c9cf03ec97fb2
Rahman <rahman.n1234567@gmail.com>
2022-10-26 23:14:17 +0400

    hrefRenderingObj

--- src/features/layouts/navi/Navi.jsx
+++ src/features/layouts/navi/Navi.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import styles from './navi.module.css';
 
+
+const hrefRenderingObj = {
+    header: 'header',
+    teleblerimiz: 'teleblerimiz',
+    dersler: 'dersler',
+    questions: 'questions'
+}
+
 const Navi = () => {
     return (
         <div id={"nav"} className={styles.navContainer}>
             <div className={styles.navIconDiv}>
             </div>
             <div></div>
-           <div className={styles.navRight}>
-               <ul className={styles.navList}>
-                   <li><a href={"#header"}>Ana shifə</a></li>
-                   <li><a href="#teleblerimiz">Tələblər</a></li>
-                   <li><a href="#dersler">Dersler</a></li>
-                   <li><a href="#questions">Suallar</a></li>
-               </ul>
-               <button className={styles.applyBtn}>Əlaqə</button>
-           </div>
+            <div className={styles.navRight}>
+                <ul className={styles.navList}>
+                    <li><a href={hrefRenderingObj.header}>Ana shifə</a></li>
+                    <li><a href={hrefRenderingObj.teleblerimiz}>Tələblər</a></li>
+                    <li><a href={hrefRenderingObj.dersler}>Dersler</a></li>
+                    <li><a href={hrefRenderingObj.questions}>Suallar</a></li>
+                </ul>
+                <button className={styles.applyBtn}>Əlaqə</button>
+            </div>
         </div>
     );
 };
