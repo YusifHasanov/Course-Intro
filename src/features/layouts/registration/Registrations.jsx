df4900b7889614f71a3a1a675b82a968fc735e6b
yusif.hasanov17@gmail.com <yusif.hasanov17@gmail.com>
2022-10-28 17:03:39 +0400

    update

--- src/features/layouts/registration/Registrations.jsx
+++ src/features/layouts/registration/Registrations.jsx
@@ -2,42 +2,51 @@ import React, {useEffect, useState} from 'react';
 import styles from './registration.module.css';
 import Go from "../../../GlobalRouting";
 import {useNavigate, useParams} from "react-router-dom";
-
 import {
     Box,
     FormControl,
     FormLabel,
     Input,
-    InputGroup,
     Stack,
-    Button,
-    Text,
-    Link, Select,
+    Button, Select,
 } from '@chakra-ui/react';
 
 
 const Registration = () => {
+
     const navigate = useNavigate();
-    const {department} =useParams();
+    const {department} = useParams();
     const [name, setName] = useState("");
     const [surname, setSurname] = useState("");
     const [email, setEmail] = useState("");
     const [departmentName, setDepartmentName] = useState("");
-    useEffect(()=>{
-        if(department==="frontend"){
+    const [price,setPrice]=useState("0");
+    useEffect(() => {
+        if (department === "frontend") {
             setDepartmentName("Frontend")
-        return;
+            return;
         }
-        if(department==="backend"){
+        if (department === "backend") {
             setDepartmentName("Backend")
             return;
         }
-        if(department==="fullstack"){
+        if (department === "fullstack") {
             setDepartmentName("Full-stack")
             return;
         }
 
-    },[])
+    }, [])
+    useEffect(()=>{
+        if(departmentName==="Frontend"){
+            setPrice("300 AZN")
+        }
+        if(departmentName==="Backend"){
+            setPrice("400 AZN")
+        }
+        if(departmentName==="Full-stack"){
+            setPrice("Frontend 300 AZN və   Backend 350 AZN")
+        }
+    },[departmentName])
     return (
         <div className={styles.registration}>
             <div className={styles.content}>
@@ -56,19 +65,25 @@ const Registration = () => {
                         p={0}>
                         <Stack spacing={4}>
                             {/*Ad*/}
-                            <FormControl  className={styles.formControl} id="firstName" isRequired>
+                            <FormControl className={styles.formControl} id="firstName" isRequired>
                                 <FormLabel>Ad</FormLabel>
-                                <Input value={name} onChange={(e)=>{setName(e.target.value)}} type="text"/>
+                                <Input value={name} onChange={(e) => {
+                                    setName(e.target.value)
+                                }} type="text"/>
                             </FormControl>
                             {/*Soyad*/}
                             <FormControl className={styles.formControl} id="lastName">
                                 <FormLabel>Soyad</FormLabel>
-                                <Input value={surname} onChange={(e)=>{setSurname(e.target.value)}} type="text"/>
+                                <Input value={surname} onChange={(e) => {
+                                    setSurname(e.target.value)
+                                }} type="text"/>
                             </FormControl>
                             {/*Email*/}
                             <FormControl className={styles.formControl} id="email" isRequired>
                                 <FormLabel>Email</FormLabel>
-                                <Input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="email"/>
+                                <Input value={email} onChange={(e) => {
+                                    setEmail(e.target.value)
+                                }} type="email"/>
                             </FormControl>
                             {/*Bolme*/}
                             <FormControl className={styles.formControl}>
@@ -78,7 +93,9 @@ const Registration = () => {
                                 </FormLabel>
                                 <Select
                                     value={departmentName}
-                                    onChange={(e)=>{setDepartmentName(e.target.value)}}
+                                    onChange={(e) => {
+                                        setDepartmentName(e.target.value)
+                                    }}
                                     className={styles.select}
                                     id={"department"}
                                     name={"department"}
@@ -87,12 +104,15 @@ const Registration = () => {
                                     shadow="sm"
                                     w="full"
                                     rounded="md">
-                                    <option   className={styles.option}>Frontend</option>
+                                    <option className={styles.option}>Frontend</option>
                                     <option className={styles.option}>Backend</option>
                                     <option className={styles.option}>Full-stack</option>
                                 </Select>
+                                <div className={styles.priceBox}>
+                                    Aylıq ödəniş: <span className={styles.price}>{price}</span>
+                                </div>
                             </FormControl>
-                            <Stack spacing={10} pt={2}>
+                            <Stack spacing={10} >
                                 <Button
                                     className={styles.submitButton}
                                     loadingText="Submitting"
